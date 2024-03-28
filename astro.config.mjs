f9da6e729f9c8baf1af5908baa7e19f49eec1157
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2024-03-28 17:23:12 -0300

    Se arregla fallo de imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from 'astro-icon';
-import { PATH } from './paths';
-
 
 // https://astro.build/config
 export default defineConfig({
