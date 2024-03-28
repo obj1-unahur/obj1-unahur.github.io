b3b55f218bf1591152ef6b5591db61237f3a4885
Branko Muruaga <muruagabranko253@gmail.com>
2024-03-28 11:06:09 -0300

    se modifico: index, layouts, header, Hero, SiteMeta, Navigation. Se agrego: CardSemana, pagina Login (no es parte de la pagina), PostCursada, MD: semana1 Se agrego la dependencia de: Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
+import { PATH } from './paths';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://obj1-unahur.github.io',
-  base: "/objetos-web",
+  base: PATH,
   compressHTML: false,
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
-})
+  integrations: [mdx(), icon(), tailwind({
+    applyBaseStyles: false
+  }), compress()]
+});
---
+++ paths.js
@@ -0,0 +1,2 @@
+export const PATH = import.meta.env.BASE_URL
+export const PATH_CURSADA = '/Cursada'
