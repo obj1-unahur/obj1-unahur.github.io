8aa2bb395803093a1f7ef0abe57afaceab44bf7a
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2024-03-28 17:11:08 -0300

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig,passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
@@ -8,6 +8,9 @@ import { PATH } from './paths';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService()
+  },
   site: 'https://obj1-unahur.github.io',
   base: '/objetos-web',
   compressHTML: false,
