d16f1d82dd6e656c32510de8c319eecbd463eec2
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2024-04-09 09:04:23 -0300

    conflictos

--- src/paths.js
+++ src/paths.js
@@ -2,3 +2,4 @@ export const PATH = '/objetos-web'
 export const PATH_CURSADA = `${PATH}/Cursada`
 export const PATH_CONTRATO_PEDAGOGICO = `${PATH}/contrato-pedagogico`
 export const PATH_VIDEOS = `${PATH}/Videos`
+export const PATH_MATERIAL = `${PATH}mMaterial`
