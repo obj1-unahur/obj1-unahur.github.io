50b95c3a30532ad48f8bb8bec097bee72f427bbc
valentinochiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-02-25 12:01:30 -0300

    cambio nombres

--- src/paths.js
+++ src/paths.js
@@ -3,4 +3,4 @@ export const PATH_CURSADA = `/cursada`
 export const PATH_CONTRATO_PEDAGOGICO = `/contrato-pedagogico`
 export const PATH_VIDEOS = `/videos`
 export const PATH_MATERIAL = `/material`
-export const PATH_SOLUCIONES = `/soluciones`
+export const PATH_SOLUCIONES = `/ejercicios-resueltos`
