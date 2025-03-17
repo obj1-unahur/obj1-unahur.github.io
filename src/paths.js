a2592555b2bad618f3a2b66ad2842eacc0184a0b
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-03-17 20:15:48 -0300

    varias cursadas

--- src/paths.js
+++ src/paths.js
@@ -1,5 +1,6 @@
 export const PATH = '/'
 export const PATH_CURSADA = `/cursada`
+export const PATH_CURSADA_2024c2 = `/cursada-2024-c2`
 export const PATH_CONTRATO_PEDAGOGICO = `/contrato-pedagogico`
 export const PATH_VIDEOS = `/videos`
 export const PATH_MATERIAL = `/material`
